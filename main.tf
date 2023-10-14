687028ba4ff4c0f5d755fe0a27b24a70f785c8b5
Ashraf Omar <ashraf.aomar@gmail.com>
2023-10-14 01:03:54 +0000

    #11 Create AWS S3 Random bucket

--- main.tf
+++ main.tf
@@ -4,6 +4,10 @@ terraform {
       source = "hashicorp/random"
       version = "3.5.1"
     }
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.16.2"
+    }
   }
 }
 
@@ -11,11 +15,29 @@ provider "random" {
   # Configuration options
 }
 
+provider "aws" {
+  # This is required only if you have some configurations to be done, otherwise no need to keep it
+}
+
+# https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
 resource "random_string" "bucket_name" {
-  length           = 16
-  special          = false
+  length = 32
+  lower = true
+  upper = false
+  special = false
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
+resource "aws_s3_bucket" "s3_bucket" {
+  # Bucket Naming Rules
+  #https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html?icmpid=docs_amazons3_console
+  bucket = random_string.bucket_name.result
 }
 
 output "random_bucket_name" {
   value = random_string.bucket_name.result
+}
+
+output "aws_s3_random_bucket" {
+  value = aws_s3_bucket.s3_bucket.bucket
 }
